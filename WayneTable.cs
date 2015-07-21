6a04b294e550f01cf5a39e17189078c550b71e2d
Moozz <fendermoo@gmail.com>
2015-07-22 00:04:09 +0700

    Move WayneTable to a new file because it is getting bigger

---
+++ WayneTable.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DrWayne {
+	public class WayneTable {
+		public WayneTable(int year, int month) {
+			_wayneTable = new List<Wayne>();
+			if (month < 1 || month > 12)
+				throw new Exception("Month must be 1-12");
+			Month = month;
+			Year = year;
+		}
+		public List<Wayne> _wayneTable;
+		
+		public int Month { get; private set; }
+		
+		public int Year { get; private set; }
+		
+		public bool AddWayneIfAcceptable(Wayne wayne) {
+			if (wayne.IsAcceptable() && 
+				!IsThisDoctorAtERYesterday(wayne.ERDoctor) &&
+				!IsThisDoctorInWayneTheLastTwoDays(wayne.ERDoctor) &&
+				!IsThisDoctorInWayneTheLastTwoDays(wayne.WardDoctor) && 
+				(wayne.OPDDoctor == null || !IsThisDoctorInWayneTheLastTwoDays(wayne.OPDDoctor))) {
+				_wayneTable.Add(wayne);
+				return true;
+			}
+			return false;			
+		}
+		
+		public override string ToString() {
+			var sb = new StringBuilder();
+			sb.AppendFormat("{0} {1, 10} {2, 10}, {3, 10}\n", "Date", "ER", "WARD", "OPD");
+			foreach (var wayne in _wayneTable) {
+				sb.AppendFormat("{0} {1, 10} {2, 10}, {3, 10}\n", 
+					wayne.WayneDate.ToString("dd MMM yyyy"), 
+					wayne.ERDoctor.Name, 
+					wayne.WardDoctor.Name, 
+					wayne.OPDDoctor == null ? "-" : wayne.OPDDoctor.Name);
+			}
+			return sb.ToString();
+		}
+		
+		public WayneTable Copy() {
+			var wayneTableCopy = new WayneTable(Year, Month);
+			wayneTableCopy._wayneTable = this._wayneTable.ToList();
+			return wayneTableCopy;
+		}
+		
+		public bool IsDone() {
+			return _wayneTable.Count == DateTime.DaysInMonth(Year, Month);
+		}
+		
+		public int GetLastWayneDay() {
+			return _wayneTable.Count;
+		}
+		
+		private bool IsThisDoctorAtERYesterday(Doctor d) {
+			return _wayneTable.Count > 0 && _wayneTable.Last().ERDoctor == d;
+		}
+		
+		private bool IsThisDoctorInWayneTheLastTwoDays(Doctor d) {
+			if (_wayneTable.Count < 2)
+				return false;
+
+			var wayneTableCopy = _wayneTable.ToList();
+			wayneTableCopy.Reverse();
+			return wayneTableCopy.Take(2).All(x => x.ERDoctor == d || x.WardDoctor == d);
+		}
+	}	
+}
