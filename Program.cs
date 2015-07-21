e9670e8f098354f3c72061acf08764cf5ecc4355
Moozz <fendermoo@gmail.com>
2015-07-22 00:20:07 +0700

    Remove trailing space

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace DrWayne {
 			var year = 2015;
 			var month = 8;
 			
-			nui.RegisterAbsence(year, month, new List<int>{ 5 , 6 });
+			nui.RegisterAbsence(year, month, new List<int>{ 5, 6 });
 			ja.RegisterAbsence(year, month, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 25, 26, 27, 28 });
 			bean.RegisterAbsence(year, month, new List<int> {2, 9, 10, 16, 17, 18, 19, 20, 21, 22 });
 			did.RegisterAbsence(year, month, new List<int> { 11, 12, 13, 14, 16 });
