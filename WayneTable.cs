52422d60c170c4ff9658a2bf689fb6d2276d2061
Moozz <fendermoo@gmail.com>
2015-07-23 23:21:37 +0700

    add fairEnough for resting

--- WayneTable.cs
+++ WayneTable.cs
@@ -11,12 +11,21 @@ namespace DrWayne {
 				throw new Exception("Month must be 1-12");
 			Month = month;
 			Year = year;
+			
+			_totalDaysInMonth = DateTime.DaysInMonth(year, month);
+			_totalWorkingDay = 0;
+			for (var i = 1; i <= _totalDaysInMonth; i++) {
+				_totalWorkingDay += new DateTime(year, month, i).IsHoliday() ? 0 : 1;
+			}
 		}
 		public List<Wayne> _wayneTable;
 		
 		public int Month { get; private set; }
 		
 		public int Year { get; private set; }
+		
+		private int _totalDaysInMonth;
+		private int _totalWorkingDay;
 		
 		public bool AddWayneIfAcceptable(Wayne wayne) {
 			if (wayne.IsAcceptable() && 
@@ -51,7 +60,7 @@ namespace DrWayne {
 		}
 		
 		public bool IsDone() {
-			return _wayneTable.Count == DateTime.DaysInMonth(Year, Month);
+			return _wayneTable.Count == _totalDaysInMonth;
 		}
 		
 		public int GetLastWayneDay() {
@@ -92,25 +101,33 @@ namespace DrWayne {
 				.Where(x => x.Doctor != null)
 				.GroupBy(x => x.Doctor);
 				
-			//  var restList = wayneGroupByDoctor.Select(g => new {
-			//  	Doctor = g.Key,
-			//  	RestOnHolidayCount = g.Count(x => x.Day.IsHoliday())
-			//  });
-				
+			var restList = wayneGroupByDoctor.Select(g => new {
+				  	Doctor = g.Key,
+				  	RestOnWorkingDayCount = _totalWorkingDay - g.Select(x => x.Day).Distinct().Count(x => !x.IsHoliday()),
+					RestOnHolidayCount = (_totalDaysInMonth - _totalWorkingDay) - g.Select(x => x.Day).Distinct().Count(x => x.IsHoliday())
+				}).ToList();
 			var countList =	wayneGroupByDoctor.Select(g => new {
 					Doctor = g.Key,
 					ERCount = g.Count(x => x.WayneType == Wayne.Type.ER),
 					WardCount = g.Count(x => x.WayneType == Wayne.Type.Ward),
 					OPDCount = g.Count(x => x.WayneType == Wayne.Type.OPD)
-				})
-				.ToList();
+				}).ToList();
 			
 			if (countList.Max(x => x.ERCount) - countList.Min(x => x.ERCount) <= 2 &&
-				countList.Max(x => x.WardCount) - countList.Min(x => x.WardCount) <= 2) {
+				countList.Max(x => x.WardCount) - countList.Min(x => x.WardCount) <= 2 &&
+				restList.Max(x => x.RestOnHolidayCount) - restList.Min(x => x.RestOnHolidayCount) <= 2 && 
+				restList.Max(x => x.RestOnWorkingDayCount) - restList.Min(x => x.RestOnWorkingDayCount) <= 4) {
 				Console.WriteLine("{0, -10} : {1, 5} {2, 5} {3, 5}", "Name", "ER", "Ward", "OPD");
 				foreach (var p in countList) {
 					Console.WriteLine("{0, -10} : {1, 5} {2, 5} {3, 5}", p.Doctor.Name, p.ERCount, p.WardCount, p.OPDCount);	
-				}	
+				}
+				Console.WriteLine();
+				Console.WriteLine("{0, -8} : {1, 10} {2, 10}", "Name", "RestWDay", "RestHoliday");
+				foreach (var p in restList) {
+					Console.WriteLine("{0, -8} : {1, 10} {2, 10}", p.Doctor.Name,
+						 p.RestOnWorkingDayCount,
+						 p.RestOnHolidayCount);
+				}
 				return true;	
 			}
 			return false;
