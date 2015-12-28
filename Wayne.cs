720866db3f5b7d65555b0338f5a6c08bf6ad2497
Moozz <fendermoo@gmail.com>
2015-12-28 14:01:18 +0700

    Add Wayne.ToString

--- Wayne.cs
+++ Wayne.cs
@@ -28,5 +28,15 @@ namespace DrWayne {
 					//(ERDoctor.Exp != DoctorExperience.Intern1 || ERDoctor.Exp != WardDoctor.Exp) &&
 					(!WayneDate.NeedOPD() || OPDDoctor != null);
 		}
+        
+        public override string ToString() {
+            return string.Format("Day : {0}, ER: {1}, WA: {2}, OPD: {3}",
+                this.WayneDate,
+                this.ERDoctor.Name,
+                this.WardDoctor.Name,
+                this.OPDDoctor != null
+                    ? this.OPDDoctor.Name
+                    : ""); 
+        }
 	}
 }
