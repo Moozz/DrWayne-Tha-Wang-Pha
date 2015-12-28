803ab9c924b6469cf425e45b5ee3bd48fd89dd39
Moozz <fendermoo@gmail.com>
2015-12-28 14:01:45 +0700

    update for Jan

--- Program.cs
+++ Program.cs
@@ -7,40 +7,33 @@ namespace DrWayne {
         public static void Main(string[] args) {
             var did = new Doctor("P' Did", DoctorExperience.Oldie, 6, 4, 0.7);
 			var nui = new Doctor("P' Nui", DoctorExperience.Oldie, 6, 6);
-			var ja = new Doctor("Ja+", DoctorExperience.Intern3, 5, 5, 0.8);
-			var bean = new Doctor("Bean", DoctorExperience.Intern3, 5, 6, 0.8);
-			var ohm = new Doctor("Ohm", DoctorExperience.Intern1, 5, 6, 0.8);
-			var tong = new Doctor("Tong", DoctorExperience.Intern1, 6, 5, 0.8);
-			_doctorList = new List<Doctor> { bean, ja, ohm, tong, nui, did };
+			var ja = new Doctor("Ja+", DoctorExperience.Intern3, 5, 5);
+			var bean = new Doctor("Bean", DoctorExperience.Intern3, 5, 6);
+			var bum = new Doctor("Bum", DoctorExperience.Oldie, 5, 6);
+			var fang = new Doctor("Fang", DoctorExperience.Intern1, 6, 5);
+			_doctorList = new List<Doctor> { bean, ja, bum, fang, nui, did };
 			
-			var year = 2015;
-			var month = 12;
-			DateTimeExtension.AddSpecialHoliday(new DateTime(year, month, 7));
-			DateTimeExtension.AddSpecialHoliday(new DateTime(year, month, 10));
-			DateTimeExtension.AddSpecialHoliday(new DateTime(year, month, 31));
+			var year = 2016;
+			var month = 1;
+			//DateTimeExtension.AddSpecialHoliday(new DateTime(year, month, 31));
 			
-			nui.RegisterAbsence(year, month, new List<int> { 5, 6, 7, 30, 31 });
-			ja.RegisterAbsence(year, month, 4, 8);
-			bean.RegisterAbsence(year, month, 6, 7);
-			//did.RegisterAbsence(year, month, new List<int> { 14, 16, 17, 18, 22, 26, 27 });
-			ohm.RegisterAbsence(year, month, 11, 13);
-			tong.RegisterAbsence(year, month, 11, 13);
+			bum.RegisterAbsence(year, month, new List<int> { 11, 14, 15, 16, 17 });
+			ja.RegisterAbsence(year, month, 15, 17);
+			bean.RegisterAbsence(year, month, new List<int>{ 8, 9, 10, 16, 17 });
+            bean.RegisterAbsence(year, month, 16, 17);
+			did.RegisterAbsence(year, month, 22, 27);
+			nui.RegisterAbsence(year, month, 26, 31);
+			fang.RegisterAbsence(year, month, 19, 26);
 			
 			var wt = new WayneTable(_doctorList, year, month);
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 5), bean, tong, did));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 6), ohm, did));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 7), tong, ohm, did));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 10), ja, bean, nui));
-			
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 11), did, nui));
-			
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 12), bean, ja, nui));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 13), ja, nui));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 19), tong, ohm, nui));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 20), ohm, nui));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 26), nui, did, ja));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 27), did, nui));
-			wt.AddFixWayne(new Wayne(new DateTime(year, month, 31), tong, bean, ohm));
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 9), bum, ja, fang));
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 10), ja, fang));
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 16), nui, fang, did));
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 17), did, nui));			
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 23), ja, bean, nui));
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 24), bean, nui));
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 30), bean, bum, did));
+			wt.AddFixWayne(new Wayne(new DateTime(year, month, 31), fang, bum));
 			
 			Console.WriteLine("Thinking...");
 			wt.Fill();
