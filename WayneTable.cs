2183e53d2c8facacb929b79be58093220ba653f1
Moozz <fendermoo@gmail.com>
2015-12-28 14:01:37 +0700

    Show what's wrong with the fixed wayne

--- WayneTable.cs
+++ WayneTable.cs
@@ -38,8 +38,10 @@ namespace DrWayne {
 		}
 		
 		public void AddFixWayne(Wayne wayne) {
-			if (!wayne.IsAcceptable())
-				throw new Exception("Your fixed wayne is not correct, please recheck it");
+			if (!wayne.IsAcceptable()) {
+                Console.WriteLine(wayne.ToString());
+                throw new Exception("Your fixed wayne is not correct, please recheck it");
+            }
 			
 			_doctorList.First(x => x == wayne.ERDoctor).ERWayne.Add(wayne.WayneDate);
 			_doctorList.First(x => x == wayne.WardDoctor).WardWayne.Add(wayne.WayneDate);
