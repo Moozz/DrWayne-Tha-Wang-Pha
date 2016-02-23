1b95457e77ea4a5892ee50429795e6f0fc290c8a
Moozz <fendermoo@gmail.com>
2016-02-23 21:55:28 +0700

    Reorder code

--- WayneTable.cs
+++ WayneTable.cs
@@ -51,48 +51,6 @@ namespace DrWayne {
 			_wayneTable.Add(wayne);
 		}
 		
-		public override string ToString() {
-			var sb = new StringBuilder();
-			sb.AppendFormat("Wayne of Month {0}, {1}\n", Month, Year);
-			sb.AppendFormat("{0, -7} {1, 10} {2, 10} {3, 10}\n", "Date", "ER", "WARD", "OPD");
-			foreach (var wayne in _wayneTable.OrderBy(x => x.WayneDate)) {
-				sb.AppendFormat("{0, -7} {1, 10} {2, 10} {3, 10}\n", 
-					wayne.WayneDate.ToString("ddd dd"), 
-					wayne.ERDoctor.Name, 
-					wayne.WardDoctor.Name, 
-					wayne.OPDDoctor == null ? "-" : wayne.OPDDoctor.Name);
-			}
-			return sb.ToString();
-		}
-		
-		public string ToCsvFormat() {
-			var sb = new StringBuilder();
-			sb.AppendLine(string.Join(",", new [] { "Date" }.Concat(_doctorList.Select(x => x.Name))));
-			foreach (var wayne in _wayneTable.OrderBy(x => x.WayneDate)) {				
-				sb.AppendLine(string.Join(",", new [] {wayne.WayneDate.ToString("ddd dd")}.Concat(_doctorList.Select(x => x.ERWayne.Contains(wayne.WayneDate) 
-						? "ER"
-						: x.WardWayne.Contains(wayne.WayneDate)
-							? "WA"
-							: x.OPDWayne.Contains(wayne.WayneDate) 
-								? "OPD"
-								: "-"))));
-			}
-			return sb.ToString();
-		}
-		
-		public void ShowResult() {
-			Console.WriteLine("{0, -10} : {1, 5} {2, 5} {3, 5}", "Name", "ER", "Ward", "OPD");
-	  		foreach (var p in _doctorList) {
-	  			Console.WriteLine("{0, -10} : {1, 5} {2, 5} {3, 5}", p.Name, p.ERWayne.Count(), p.WardWayne.Count(), p.OPDWayne.Count());	
-	  		}
-			Console.WriteLine("{0}", ToCsvFormat());
-			//Console.WriteLine("{0}", ToString());
-			//Console.WriteLine("Tireness level at the end of the month");
-			//Console.WriteLine(string.Join("\n", _doctorList.Select(x => x.Name + " : " + x.Tireness)));
-			//Console.WriteLine("=== END ===\n");
-			Console.ReadLine();
-		}
-		
 		private WayneTable Copy() {
 			var wayneTableCopy = new WayneTable(_doctorList, Year, Month);
 			wayneTableCopy._wayneTable = this._wayneTable.ToList();
@@ -128,31 +86,8 @@ namespace DrWayne {
 			//}
 			//return false;
 		}
-		
-        private void ShowFairness() {
-            var doctorListWithoutHandicap = _doctorList.Where(d => d.Factor == 1);
-			var restList = doctorListWithoutHandicap
-				.Select(x => new {
-					Name = x.Name,
-					WorkDayList = x.GetAllWayneDate()
-				})
-				.Select(x => new {
-				  	Name = x.Name,
-				  	RestOnWorkingDayCount = _totalWorkingDay - x.WorkDayList.Count(y => !y.IsHoliday()),
-					RestOnHolidayCount = (_totalDaysInMonth - _totalWorkingDay) - x.WorkDayList.Count(y => y.IsHoliday())
-				}).ToList();
 
-            Console.WriteLine();
-            Console.WriteLine("{0, -8} : {1, 10} {2, 10} {3, 10}", "Name", "RestWDay", "RestHoliday", "Sum");
-            foreach (var p in restList) {
-                Console.WriteLine("{0, -8} : {1, 10} {2, 10} {3, 10}", p.Name,
-                        p.RestOnWorkingDayCount,
-                        p.RestOnHolidayCount,
-                        p.RestOnHolidayCount + p.RestOnWorkingDayCount);
-            }
-        }
-        
-		public void Fill() {
+        public void Fill() {
 			if (IsDone()) {
                 ShowFairness();
 				if (!FairEnough()) return;
@@ -211,6 +146,72 @@ namespace DrWayne {
 				erDoctor.Tireness -= erTireness;
 				erDoctor.ERWayne.Remove(currentDate);
 			}
+		}
+
+        private void ShowFairness() {
+            var doctorListWithoutHandicap = _doctorList.Where(d => d.Factor == 1);
+			var restList = doctorListWithoutHandicap
+				.Select(x => new {
+					Name = x.Name,
+					WorkDayList = x.GetAllWayneDate()
+				})
+				.Select(x => new {
+				  	Name = x.Name,
+				  	RestOnWorkingDayCount = _totalWorkingDay - x.WorkDayList.Count(y => !y.IsHoliday()),
+					RestOnHolidayCount = (_totalDaysInMonth - _totalWorkingDay) - x.WorkDayList.Count(y => y.IsHoliday())
+				}).ToList();
+
+            Console.WriteLine();
+            Console.WriteLine("{0, -8} : {1, 10} {2, 10} {3, 10}", "Name", "RestWDay", "RestHoliday", "Sum");
+            foreach (var p in restList) {
+                Console.WriteLine("{0, -8} : {1, 10} {2, 10} {3, 10}", p.Name,
+                        p.RestOnWorkingDayCount,
+                        p.RestOnHolidayCount,
+                        p.RestOnHolidayCount + p.RestOnWorkingDayCount);
+            }
+        }
+        
+        
+		public override string ToString() {
+			var sb = new StringBuilder();
+			sb.AppendFormat("Wayne of Month {0}, {1}\n", Month, Year);
+			sb.AppendFormat("{0, -7} {1, 10} {2, 10} {3, 10}\n", "Date", "ER", "WARD", "OPD");
+			foreach (var wayne in _wayneTable.OrderBy(x => x.WayneDate)) {
+				sb.AppendFormat("{0, -7} {1, 10} {2, 10} {3, 10}\n", 
+					wayne.WayneDate.ToString("ddd dd"), 
+					wayne.ERDoctor.Name, 
+					wayne.WardDoctor.Name, 
+					wayne.OPDDoctor == null ? "-" : wayne.OPDDoctor.Name);
+			}
+			return sb.ToString();
+		}
+		
+		public string ToCsvFormat() {
+			var sb = new StringBuilder();
+			sb.AppendLine(string.Join(",", new [] { "Date" }.Concat(_doctorList.Select(x => x.Name))));
+			foreach (var wayne in _wayneTable.OrderBy(x => x.WayneDate)) {				
+				sb.AppendLine(string.Join(",", new [] {wayne.WayneDate.ToString("ddd dd")}.Concat(_doctorList.Select(x => x.ERWayne.Contains(wayne.WayneDate) 
+						? "ER"
+						: x.WardWayne.Contains(wayne.WayneDate)
+							? "WA"
+							: x.OPDWayne.Contains(wayne.WayneDate) 
+								? "OPD"
+								: "-"))));
+			}
+			return sb.ToString();
+		}
+		
+		public void ShowResult() {
+			Console.WriteLine("{0, -10} : {1, 5} {2, 5} {3, 5}", "Name", "ER", "Ward", "OPD");
+	  		foreach (var p in _doctorList) {
+	  			Console.WriteLine("{0, -10} : {1, 5} {2, 5} {3, 5}", p.Name, p.ERWayne.Count(), p.WardWayne.Count(), p.OPDWayne.Count());	
+	  		}
+			Console.WriteLine("{0}", ToCsvFormat());
+			//Console.WriteLine("{0}", ToString());
+			//Console.WriteLine("Tireness level at the end of the month");
+			//Console.WriteLine(string.Join("\n", _doctorList.Select(x => x.Name + " : " + x.Tireness)));
+			//Console.WriteLine("=== END ===\n");
+			Console.ReadLine();
 		}
 	}	
 }
